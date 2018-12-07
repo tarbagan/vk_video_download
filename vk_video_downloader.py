9a36b7ea1748c1d05e2a7195a81668cfaabe4702
Irgit <36793333+tarbagan@users.noreply.github.com>
2018-12-07 12:22:50 +0700

    Update vk_video_downloader.py

--- vk_video_downloader.py
+++ vk_video_downloader.py
@@ -8,6 +8,8 @@ import os
 ZAPROS = "Цой клип" #Что ищем
 FOLDER = "h:/VK_Video_donloads/" #Куда качать
 СNT_VIDEO = "10" #Сколько роликов скачать, максимумм 200
+HD_ON = "1" #1- искать только HD, 0-искать всё
+FILTER_ON = "short" #short — только короткие видеозаписи, long — только длинные видеозаписи.
 
 vk_session = vk_api.VkApi( 'YOU LOGIN VK', 'YOU PASSWORD' )
 vk_session.auth()
@@ -17,7 +19,7 @@ patch_folder = FOLDER+ZAPROS
 if not os.path.isdir(patch_folder):
     os.makedirs(patch_folder)
 
-for data in vk.video.search(q=ZAPROS, sort=0, count = СNT_VIDEO, hd=1, filters="short")["items"]:
+for data in vk.video.search(q=ZAPROS, sort=0, count = СNT_VIDEO, hd=HD_ON, filters=FILTER_ON)["items"]:
     name = (re.sub( "^\s+|\n|\r|\s+$", '', data["title"]))
     #print (data["player"]) #ссылка на плеер
     player = (data["player"])
